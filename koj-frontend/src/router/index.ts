8ddd4a310c04cfaedf44383412bc05e9dd098cec
KyLenMou <2729269812@qq.com>
2024-09-04 11:35:15 +0800

    fix(FE): 题管理目页面跳转时页面保持在最上方

--- koj-frontend/src/router/index.ts
+++ koj-frontend/src/router/index.ts
@@ -9,6 +9,13 @@ import { ElMessage } from "element-plus";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 // todo /admin/tag刷新后回到了/admin/problemset
 export function setupRouterGuard() {
